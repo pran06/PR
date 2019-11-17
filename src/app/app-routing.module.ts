909a1715bed6ba2d5e4a58cc619423f49c238e69
Praveen Reddy <praveenreddy.anugu@saabgroup.com>
2019-11-17 21:30:26 +0100

    Init setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [ { path: 'welcome', component: WelcomeComponent },
+{path:'products', component: ProductListComponent},
+{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
+{ path: '**', redirectTo: 'welcome', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
